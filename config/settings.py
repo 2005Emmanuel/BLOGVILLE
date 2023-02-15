9e359b4274d3c94f85c766a03f9edb6ef4caec95
Your Name <you@example.com>
2023-02-15 02:03:34 +0100

    change

--- config/settings.py
+++ config/settings.py
@@ -181,3 +181,8 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 LOGIN_REDIRECT_URL='/'
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://blogville-production.up.railway.app/",
+   
+    ]
+PYTHON_VERSION = 3.7
