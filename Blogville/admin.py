7dbc8f9c73ecee1248868616a713a179c2c9d88f
Your Name <you@example.com>
2023-02-18 18:22:24 +0100

    changes

--- Blogville/admin.py
+++ Blogville/admin.py
@@ -5,7 +5,9 @@ from .models import Comment
 from .models import Follower_Following
 # from .models import Like_Unlike
 # Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class registerAdmin(admin.ModelAdmin):
+ raw_id_fields = ['user']
 admin.site.register(Blogpost)
 admin.site.register(Comment)
 admin.site.register(Follower_Following)
